c3698aa0c11237e9cf59858ce496123cd5161f11
Will Theiler <will@Wills-MacBook-Air.local>
2020-12-09 12:06:00 -0500

    1st push with starter db calls

--- db/index.js
+++ db/index.js
@@ -1,13 +1,117 @@
 // Connect to DB
 const { Client } = require('pg');
-const DB_NAME = 'change-this-name'
-const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
+const DB_NAME = 'localhost:5432/link-badger-dev'
+const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
 const client = new Client(DB_URL);
 
 // database methods
 
+
+// I need a function to grab all the links in the LINKS table and return NAME, DESCRIPTION, CLICK_COUNT and grab the tags from the TAGS table to return to the front end to display on the UI
+async function getAllLinks() {
+  try {
+    const { rows: links } = await client.query(`
+        SELECT * 
+        FROM links l;
+    
+    
+    `)
+    console.log("all links for getAllLinks:", links)
+    return links
+
+  } catch (error) {
+    throw error
+  }
+}
+
+
+
+// to get the tags for each link:
+// SELECT *
+// FROM links l
+// JOIN tags t ON t."linksId" = l.id
+
+
+
+// I need a function to insert a new link into the links table.  Will need NAME, URL & DESCRIPTION
+// createLink
+
+async function createLink({ name, url, description }) {
+  // const { name, description } = linkInfo
+  try {
+    const { rows: [link] } = await client.query(`
+    INSERT 
+    INTO links(name, url, description)
+    VALUES($1, $2, $3)
+    RETURNING *;
+  `, [name, url, description])
+    // console.log(link)
+    return link
+  } catch (error) {
+    throw error
+  }
+
+}
+
+
+
+
+
+
+// I need a function to insert a new Tag into the Tags (& LinkTags) table?
+async function createTag(linksId, content) {
+  console.log("link.id passed in from dbtest:", linksId)
+  try {
+    const { rows: newTag } = await client.query(`
+    INSERT
+    INTO tags("linksId", content)
+    VALUES($1, $2)
+    RETURNING*;
+    
+    `, [linksId, content])
+    return newTag
+  } catch (error) {
+    throw (error)
+  }
+}
+
+
+
+
+
+
+
+// Let's start with the ability to DELETE an entire LINK - if we can we can do a POST to edit (for both Links & Tags) as a stretch goal
+// deleteLink
+
+async function deleteLink({ linkId }) {
+
+  try {
+    const { rows: link } = await client.query(`
+    DELETE 
+    FROM links
+    WHERE id=${linkId}
+    RETURNING *;
+    `)
+
+    console.log("this link has been deleted:", link)
+    return link
+  }
+  catch (error) {
+    throw error
+  }
+}
+
+
+
+
+
 // export
 module.exports = {
   client,
+  getAllLinks,
+  createLink,
+  createTag,
+  deleteLink
   // db methods
 }
--- db/init_db.js
+++ db/init_db.js
@@ -1,31 +1,139 @@
 // code to build and initialize DB goes here
 const {
-  client
+  client,
+  getAllLinks,
+  createLink,
+  createTag,
+  deleteLink
   // other db methods 
 } = require('./index');
 
+
 async function buildTables() {
   try {
+
+
     client.connect();
 
     // drop tables in correct order
+    try {
+      console.log('Starting to drop tables...');
+
+      client.query(`
+        DROP TABLE IF EXISTS tags;
+        DROP TABLE IF EXISTS links;
+      `);
+
+      console.log('Finished dropping tables!');
+    } catch (error) {
+      console.error('Error while dropping tables!');
+
+      throw error;
+    }
 
     // build tables in correct order
+
 
   } catch (error) {
     throw error;
   }
+
+  try {
+    console.log('Starting to construct tables...');
+
+    await client.query(`
+      CREATE TABLE links(
+        id SERIAL PRIMARY KEY,
+        name varchar(255) NOT NULL,
+        url varchar(255) NOT NULL,
+        description TEXT NOT NULL,
+        "createDate" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+      );
+
+      CREATE TABLE tags(
+        id SERIAL PRIMARY KEY,
+        "linksId" INTEGER REFERENCES links(id),
+        content TEXT NOT NULL
+      )
+    `);
+
+    console.log('Finished constructing tables!');
+  } catch (error) {
+    console.error('Error constructing tables!');
+
+    throw error;
+  }
+
 }
 
 async function populateInitialData() {
   try {
     // create useful starting data
+    console.log("Creating test links data...")
+
+    const linkOne = await createLink({
+      name: "FaveLink",
+      url: "www.google.com",
+      description: "This is by far my favorite Link on the internet."
+    })
+
+    const linkTwo = await createLink({
+      name: "SecondFaveLink",
+      url: "www.zillow.com",
+      description: "looking for or dreaming about your next abode? - check this site out!"
+    })
+    console.log("success creating links!")
+    console.log("linkOne is:", linkOne)
+    console.log("linkTwo is:", linkTwo)
+    return [linkOne, linkTwo]
   } catch (error) {
     throw error;
   }
+
 }
+
+async function populateInitialTagsData(initialLinks) {
+
+  const [linkOne, linkTwo] = initialLinks
+
+  try {
+    console.log("creating tags initial data...")
+
+    const tagOne = await createTag(
+      linkOne.id, {
+      content: "search"
+    }
+    )
+
+    const tagTwo = await createTag(
+      linkOne.id, {
+      content: "big brother"
+    }
+    )
+
+    const tagThree = await createTag(
+      linkTwo.id, {
+      content: "educational"
+    }
+    )
+
+    console.log("success creating tags!")
+    console.log("tagOne is:", tagOne)
+    console.log("tagTwo is:", tagTwo)
+    console.log("tagThree is:", tagThree)
+    return [tagOne, tagTwo, tagThree]
+
+  } catch (error) {
+    throw error
+  }
+}
+
+
+
 
 buildTables()
   .then(populateInitialData)
+  .then(populateInitialTagsData)
+  .then(getAllLinks)
   .catch(console.error)
   .finally(() => client.end());
