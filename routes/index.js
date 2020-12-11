2ae3dbc57b05632b0c246e30cdc510fc57b3e9f2
Will Theiler <will@Wills-MacBook-Air.local>
2020-12-10 22:35:50 -0500

    routes set up with initial links call returning test links

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const apiRouter = require('express').Router();
+const { getAllLinks } = require('../db')
 
-apiRouter.get("/", (req, res, next) => {
-  res.send({
-    message: "API is under construction!"
-  });
-});
+
+
+apiRouter.get('/', async (req, res) => {
+  try {
+    console.log("in my try block of /links")
+    const links = await getAllLinks()
+    res.send({ links })
+  } catch (error) {
+    throw error
+  }
+})
+
+
 
 module.exports = apiRouter;
