d4b29129553ece922f680714a5ac605f5a2cfeff
Will Theiler <will@Wills-MacBook-Air.local>
2020-12-19 13:59:51 -0500

    heroku again after client update

--- db/index.js
+++ db/index.js
@@ -2,7 +2,9 @@
 const { Client } = require('pg');
 const DB_NAME = 'localhost:5432/link-badger-dev'
 const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
-const client = new Client(DB_URL);
+// const client = new Client(DB_URL);
+
+const client = new Client({ connectionString: DB_URL, ssl: { rejectUnauthorized: false, }, });
 
 // database methods
 
